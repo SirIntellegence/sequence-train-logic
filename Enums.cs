9aaf1ac74671c5f68bf4e0090832863cea11aa90
sirintellegence <SirIntellegence@users.noreply.github.com>
2015-09-05 17:58:59 -0600

    Converted to a C# project for simplicity...

---
+++ Enums.cs
@@ -0,0 +1,19 @@
+
+
+namespace sequencetrainlogic{
+	public enum TrackType{
+		STRAIGHT,
+		CURVED,
+		DOUBLE_STRAIGHT,
+		DOUBLE_CURVED
+	}
+	public enum TrackEnds{
+		A,B,NONE
+	}
+	public enum TrackSide{
+		NORTH,
+		EAST,
+		SOUTH,
+		WEST
+	}
+}
---
+++ SequenceTrainEngine.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace sequencetrainlogic {
+	public class SequenceTrainEngine {
+		private EngineOptions engineOptions;
+		public SequenceTrainEngine() {
+		}
+		public SequenceTrainEngine(EngineOptions options){
+			engineOptions = options;
+		}
+	}
+}
+
